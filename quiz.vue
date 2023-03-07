0e0fc74c8eb0f5113ee7d3841961f8ff7c133d11
Nick Cruz <nickcruz920@gmail.com>
2023-03-07 16:00:15 -0500

    Began with comments and creation of #app

--- quiz.vue
+++ quiz.vue
@@ -1,1 +1,5 @@
-
+<!-- This app will be an interactive/well designed quiz app... User is asked a series of questions.. if answer correctly then move onto next page. Use alot of conditional rendering -->
+<template>
+  <div id="app">
+    <h2>Quiz App</h2>
+    
